4ce13481894d4938c5f8a40318b4c68530c1d775
刘颖斌 <liuyingbin@weidian.com>
2021-11-20 14:55:36 +0800

    prettier

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,5 @@ let version: string = "0.0.1";
 export default (): string => {
   return `this version is ${version}`;
 };
+
+export const _version = '0.0.1'
---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+import { _version }  from '../src/main';
+
+describe('version', () => {
+    console.log('version' , _version)
+  test('version', () => {
+    // expect(_version).toBe('0.0.1')
+  });
+});
